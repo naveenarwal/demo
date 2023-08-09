8e5eb12b3ec8f6b96e0c814afc64ac743b79856a
naveenarwal <naveenarwal@gmail.com>
2023-08-09 14:16:08 +0530

    first commit

--- Employee.java
+++ Employee.java
@@ -1,3 +1,20 @@
-class Employee{
-  String name="naveen";
-}
+class Test
+    {
+     	public static void main(String[] args)
+     	{
+     		Linklist l=new Linklist();
+     		l.addAtFirst(100);
+            l.addAtFirst(200);
+            l.addAtFirst(300);
+            l.addAtFirst(400);
+           
+            l.addAtLast(20);
+            l.addAtLast(30);
+         
+          System.out.println(l);      
+         
+        }
+    }
+
+
+
